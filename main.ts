234f7bcc730cfddb1d8fbeb8433b3e5581ae5cda
jackbassman <79383566+jackbassman@users.noreply.github.com>
2021-02-22 17:12:01 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(Ftemp)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(input.temperature())
+    basic.showNumber(tempLOCAL)
 })
+let tempLOCAL = 0
 let Ftemp = 0
 radio.setGroup(1)
 basic.forever(function () {
-	
+    tempLOCAL = input.temperature()
+    tempLOCAL = tempLOCAL * 2
+    tempLOCAL = tempLOCAL + 30
 })
